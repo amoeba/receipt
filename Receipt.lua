aeb27f2c5daea0a6159f5031465159b615225d0f
Bryce Mecum <petridish@gmail.com>
2009-01-09 00:30:01 -0800

    Looks like things are working again

--- Receipt.lua
+++ Receipt.lua
@@ -4,9 +4,8 @@ f:RegisterEvent("MERCHANT_SHOW")
 f:RegisterEvent("MERCHANT_CLOSED")
 
 -- UTILITY
-
-local function Print(msg) ChatFrame1:AddMessage(msg); end
-local function PrintF(fmsg, ...) ChatFrame1:AddMessage(string.format("|cFF33FF99Receipt|r "..fmsg, ...)); end
+local initial_money = 0
+local function PrintF(fmsg, ...) ChatFrame1:AddMessage(string.format("|cFFFFFF00"..fmsg.."|r", ...)); end
 
 --[[  FormatMoney is a slight modification of code snippet by Tekkub
       (http://tekkub.net) which was posted at 
@@ -16,20 +15,25 @@ local function PrintF(fmsg, ...) ChatFrame1:AddMessage(string.format("|cFF33FF99
 local function FormatMoney(money)
   if not money then return end
   
-  local money = money
+  local money = abs(money)
   local g, s, c = floor(money/10000), floor((money/100)%100), money%100
   
   if g > 0 then return string.format("|cffffd700%dg|cffc7c7cf%02ds|cffeda55f%02dc", g, s, c)
   elseif s > 0 then return string.format("|cffc7c7cf%ds|cffeda55f%02dc", s, c)
-  else return string.format("|cffc7c7cf%dc", c) end
+  else return string.format("|cffeda55f%dc", c) end
 end
 
 function f:MERCHANT_SHOW()
-  Print("MERCHANT_SHOW")
-  local money = GetMoney()
-  Print(money)
-  PrintF("%s", FormatMoney(money))
+  initial_money = GetMoney()
 end
 
 function f:MERCHANT_CLOSED()
+  local delta = GetMoney() - initial_money
+  
+  if delta == 0 then return
+  elseif delta > 0 then PrintF("You earned %s", FormatMoney(delta))
+  else PrintF("You spent %s", FormatMoney(delta)) end
+  
+  -- Reset so MERCHANT_CLOSED being called twice doesn't make us spam the user
+  initial_money = GetMoney()
 end
